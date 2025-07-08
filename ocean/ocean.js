b86ed4c5c01952cd0a6b76cf01c3e0e65bc61d36
Sevil116 <156057402+Sevil116@users.noreply.github.com>
2025-07-08 16:22:15 +0800

    Add files via upload

---
+++ ocean/ocean.js
@@ -0,0 +1,45 @@
+gsap.from("#head", {delay: 0.3, duration:2, y: 200, ease: "power2.inOut", opacity:0})
+gsap.from("#container-countdown", {delay: 0.3, duration:3, y: -200, ease: "power2.inOut", opacity:0})
+gsap.from("#twoButtons", {delay: 1, duration:3, y: -200, ease: "power2.inOut", opacity:0})
+
+const timer = 5;
+let amountTime = timer * 60;
+let timerId;
+
+const start = document.querySelector("#myButton");
+
+start.addEventListener("click", startTimer);
+
+function startTimer() {
+    clearInterval(timerId);
+    timerId = setInterval(calculateTime, 1000);
+    document.querySelector("#player").play()
+    document.querySelector("#myVideo").play()
+}
+
+function calculateTime() {
+    const countdown = document.querySelector("#countdown");
+    let minutes = Math.floor(amountTime / 60);
+    let seconds = amountTime % 60;
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    if (amountTime <= 0) {
+        stopTimer();
+        amountTime = 0;
+    }
+
+    countdown.textContent = `${minutes}:${seconds}`;
+    amountTime--;
+}
+
+function stopTimer() {
+    clearInterval(timerId);
+}
+
+const home = document.querySelector("#home");
+  home.addEventListener("click", () => {
+  document.location.assign("https://calm-and-breath.glitch.me");
+});
